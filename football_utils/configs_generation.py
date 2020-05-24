5d8ff3161b472f57d704f6e6e5a13a972cdab05e
ICalofir <calofir.ionut@yahoo.com>
2020-05-24 13:57:10 +0300

    Add configs generation

---
+++ football_utils/configs_generation.py
@@ -0,0 +1,58 @@
+import os
+import numpy as np
+from sklearn.model_selection import train_test_split
+from absl import logging
+
+class ConfigsGeneration:
+    def __init__(self, dataset_path):
+        self._dataset_path = dataset_path
+        self._train_percentage = 0.90
+
+        self.cls_to_id = {
+                'no_action': 0,
+                'pass': 1,
+                'shot': 2
+        }
+
+        os.makedirs(os.path.join(self._dataset_path, 'configs'), exist_ok=True)
+
+    def _write_csv(self, X, y, mode):
+        with open(os.path.join(self._dataset_path, 'configs', mode) + '.csv', 'w') as f:
+            for X_elem, y_elem in zip(X, y):
+                f.write('{} {}\n'.format(X_elem, y_elem))
+
+            for cls, cls_id in self.cls_to_id.items():
+                logging.info('{} {} num examples: {}'.format(mode.title(), cls, np.sum(y == cls_id)))
+
+    def generate(self):
+        X = []
+        y = []
+
+        classes = sorted([cls for cls in os.listdir(self._dataset_path) if cls != 'configs'])
+
+        for cls in classes:
+            cls_path = os.path.join(self._dataset_path, cls)
+            for vid in sorted(os.listdir(cls_path)):
+                vid_path = os.path.join(cls, vid)
+
+                X.append(vid_path)
+                y.append(self.cls_to_id[cls])
+
+        X = np.array(X)
+        y = np.array(y)
+
+        X_train, X_valtest, y_train, y_valtest = train_test_split(X, y,
+                                                                  train_size=self._train_percentage,
+                                                                  random_state=42,
+                                                                  stratify=y)
+
+        X_val, X_test, y_val, y_test = train_test_split(X_valtest, y_valtest,
+                                                        test_size=0.5,
+                                                        random_state=42,
+                                                        stratify=y_valtest)
+
+        self._write_csv(X_train, y_train, mode='train')
+        self._write_csv(X_val, y_val, mode='val')
+        self._write_csv(X_test, y_test, mode='test')
+
+        logging.info('Done!')
--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from absl import app
 from absl import logging
 
 from football_utils.dataset_generation import DatasetGeneration
+from football_utils.configs_generation import ConfigsGeneration
 
 FLAGS = flags.FLAGS
 
 if not FLAGS.is_parsed():
-    flags.DEFINE_enum('phase', None, ['generate_dataset'], 'Phase to run.')
+    flags.DEFINE_enum('phase', None, ['generate_dataset', 'generate_configs'], 'Phase to run.')
 
     # DATASET GENERATION
     flags.DEFINE_enum('dataset_generation_name', None, ['video_recognition', 'expected_goals', 'heatmap'], 'What dataset to generate.')
@@ -15,6 +16,9 @@ if not FLAGS.is_parsed():
     flags.DEFINE_string('dataset_generation_path', '/mnt/storage1/football_dataset/raw_dataset', 'Path to the raw dataset.')
     flags.DEFINE_string('dataset_generation_output_path', None, 'Path to the output dataset.')
     flags.DEFINE_bool('dataset_generation_downscale_videos', True, 'Whether to downscale the videos.')
+
+    # CONFIGS GENERATION
+    flags.DEFINE_string('configs_generation_dataset_path', None, 'Path to classes examples.')
 
 def main(_):
     if FLAGS.phase is None:
@@ -35,6 +39,13 @@ def main(_):
                                                FLAGS.dataset_generation_video_recognition_classes,
                                                FLAGS.dataset_generation_downscale_videos)
         dataset_generation.generate()
+    elif FLAGS.phase == 'generate_configs':
+        if FLAGS.configs_generation_dataset_path is None:
+            logging.error('Please select the path to the dataset.')
+            raise Exception('"configs_generation_dataset_path" flag is none!')
+        configs_generation = ConfigsGeneration(FLAGS.configs_generation_dataset_path)
+
+        configs_generation.generate()
 
     logging.info('Done!')
 
